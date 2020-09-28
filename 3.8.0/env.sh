ac656895ee302c9e3df52301866ff8982e249404
xuhuisheng <xyz20003@gmail.com>
2020-09-28 10:55:09 +0800

    download sources

--- 3.8.0/env.sh
+++ 3.8.0/env.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 export ROCM_DIR=/opt/rocm
-export ROCM_GIT_REPO=/home/work/rocm
+export ROCM_GIT_REPO=/home/work/ROCm
 export PATH=/opt/rocm/bin:/opt/rocm/llvm/bin:/opt/rocm/hip/bin:$PATH
 
