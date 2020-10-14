5f5d31574bdce5cc066cae197d27c2d57fd55bf7
xuhuisheng <xuhuisheng@meicai.cn>
2020-10-14 14:37:14 +0800

    make fortran

---
+++ 3.8.0/ubuntu1804/53.rocsparse.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -e
+
+mkdir -p $ROCM_BUILD_DIR/rocsparse
+cd $ROCM_BUILD_DIR/rocsparse
+pushd .
+
+CXX=$ROCM_INSTALL_DIR/bin/hipcc cmake \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DCPACK_SET_DESTDIR=OFF \
+    -DCMAKE_INSTALL_PREFIX=rocsparse-install \
+    -DCPACK_PACKAGING_INSTALL_PREFIX=$ROCM_INSTALL_DIR \
+    -DROCM_PATH=$ROCM_INSTALL_DIR \
+    $ROCM_GIT_DIR/rocSPARSE
+make package -j${nproc}
+make package
+sudo dpkg -i *.deb
+
+popd
+
