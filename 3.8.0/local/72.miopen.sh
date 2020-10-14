fafd5d396595fea3b1f221aa1c53324002bc1c4f
xuhuisheng <xuhuisheng@meicai.cn>
2020-10-14 15:10:09 +0800

    miopen

---
+++ 3.8.0/local/72.miopen.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -e
+
+mkdir -p $ROCM_BUILD_DIR/miopen
+cd $ROCM_BUILD_DIR/miopen
+pushd .
+
+CXX=$ROCM_INSTALL_DIR/llvm/bin/clang++ cmake \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DCPACK_SET_DESTDIR=OFF \
+    -DCPACK_PACKAGING_INSTALL_PREFIX=$ROCM_INSTALL_DIR \
+    -DCMAKE_INSTALL_PREFIX=$ROCM_INSTALL_DIR \
+    -G Ninja \
+    $ROCM_GIT_REPO/MIOpen
+ninja
+ninja package
+sudo dpkg -i *.deb
+# make package -j${nproc}
+
+popd
+
