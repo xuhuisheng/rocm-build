c2e3a7bd17f289e2508d6eaa9cbb9d0ccc3f3907
xuhuisheng <xuhuisheng@meicai.cn>
2020-10-28 13:53:27 +0800

    amdgpu_targets

--- local/22.rocblas.sh
+++ local/22.rocblas.sh
@@ -23,9 +23,10 @@ rm -rf $ROCM_GIT_DIR/rocBLAS/library/src/blas3/Tensile/Logic/asm_full/r9nano*
 START_TIME=`date +%s`
 
 CXX=$ROCM_INSTALL_DIR/bin/hipcc cmake -lpthread \
+    -DAMDGPU_TARGETS=$AMDGPU_TARGETS \
     -DROCM_PATH=$ROCM_INSTALL_DIR \
     -DTensile_LOGIC=asm_full \
-    -DTensile_ARCHITECTURE=gfx803 \
+    -DTensile_ARCHITECTURE=$AMDGPU_TARGETS \
     -DTensile_CODE_OBJECT_VERSION=V3 \
     -DCMAKE_BUILD_TYPE=Release \
     -DTensile_TEST_LOCAL_PATH=/home/work/rocm-deps/Tensile \
