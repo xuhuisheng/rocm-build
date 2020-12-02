2fbe3960dde75e054f27cedf73a37a1abe83a1b3
xuhuisheng <xyz20003@gmail.com>
2020-12-02 15:59:25 +0800

    half

--- 67.half.sh
+++ 67.half.sh
@@ -6,15 +6,15 @@ mkdir -p $ROCM_BUILD_DIR/half
 cd $ROCM_BUILD_DIR/half
 pushd .
 
-git clone https://github.com/ROCmSoftwarePlatform/half
+# git clone https://github.com/ROCmSoftwarePlatform/half
 
 START_TIME=`date +%s`
 
 cp -R ../../half_1.12.0_amd64 $ROCM_BUILD_DIR/half/
 
-mkdir -p $ROCM_BUILD_DIR/half/half_1.12.0_amd64/opt/rocm/
+# mkdir -p $ROCM_BUILD_DIR/half/half_1.12.0_amd64/opt/rocm/
 
-cp -R $ROCM_BUILD_DIR/half/half/include $ROCM_BUILD_DIR/half/half_1.12.0_amd64/opt/rocm/
+# cp -R $ROCM_BUILD_DIR/half/half/include $ROCM_BUILD_DIR/half/half_1.12.0_amd64/opt/rocm/
 
 dpkg -b half_1.12.0_amd64
 
