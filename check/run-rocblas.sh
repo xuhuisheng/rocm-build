92bdc8d43e5b8eb58d03d4dee0a7d630222b0179
xuhuisheng <xuhuisheng@meicai.cn>
2020-12-10 07:29:56 +0800

    src dir

--- check/run-rocblas.sh
+++ check/run-rocblas.sh
@@ -4,7 +4,7 @@ export PATH=/opt/rocm/bin:$PATH
 
 mkdir -p build
 
-hipcc -D__HIP_PLATFORM_HCC__ -lrocblas -L/opt/rocm/lib test_rocblas.cpp -o build/test_rocblas
+hipcc -D__HIP_PLATFORM_HCC__ -lrocblas -L/opt/rocm/lib src/test_rocblas.cpp -o build/test_rocblas
 
 ROCBLAS_LAYER=0xf TENSILE_DB=0xffff ./build/test_rocblas
 
