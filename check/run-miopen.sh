75611c7dbb41e14951ab86b56a065db27f5a242c
xuhuisheng <xyz20003@gmail.com>
2021-01-14 10:53:52 +0800

    simplify unnessisory param

--- check/run-miopen.sh
+++ check/run-miopen.sh
@@ -4,7 +4,7 @@ export PATH=/opt/rocm/bin:$PATH
 
 mkdir -p build
 
-hipcc -D__HIP_PLATFORM_HCC__ -I./src/ -L/opt/rocm/lib/ -lMIOpen -std=c++11 src/test_miopen.cpp src/libbmp.c -o build/test_miopen
+hipcc -D__HIP_PLATFORM_HCC__ -L/opt/rocm/lib/ -lMIOpen src/test_miopen.cpp -o build/test_miopen
 
 MIOPEN_ENABLE_LOGGING=0 MIOPEN_LOG_LEVEL=0 ./build/test_miopen
 
