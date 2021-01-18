0d9357bf6c506152c2d3c1fa71fea18d92c191ec
xuhuisheng <xyz20003@gmail.com>
2021-01-18 17:03:01 +0800

    dkms

--- 69.rock-dms.sh
+++ 69.rock-dms.sh
@@ -1,24 +1,24 @@
 
-mkdir -p build/rock-dkms
+mkdir -p $ROCM_BUILD_DIR/rock-dkms
 
-cd build/rock-dkms
+cd $ROCM_BUILD_DIR/rock-dkms
 
-cp -R ~/ROCm/ROCK-Kernel-Driver/drivers/gpu/drm/amd .
+cp -R $ROCM_GIT_DIR/ROCK-Kernel-Driver/drivers/gpu/drm/amd .
 
-cp ~/ROCm/ROCK-Kernel-Driver/drivers/gpu/drm/amd/dkms/dkms.conf .
+cp $ROCM_GIT_DIR/ROCK-Kernel-Driver/drivers/gpu/drm/amd/dkms/dkms.conf .
 
 ln -s amd/dkms/Makefile Makefile
 
 mkdir -p firmware
 mkdir -p include
 
-cp -R ~/ROCm/ROCK-Kernel-Driver/include/drm include/
-cp -R ~/ROCm/ROCK-Kernel-Driver/include/kcl include/
-cp -R ~/ROCm/ROCK-Kernel-Driver/include/linux include/
-cp -R ~/ROCm/ROCK-Kernel-Driver/include/uapi include/
+cp -R $ROCM_GIT_DIR/ROCK-Kernel-Driver/include/drm include/
+cp -R $ROCM_GIT_DIR/ROCK-Kernel-Driver/include/kcl include/
+cp -R $ROCM_GIT_DIR/ROCK-Kernel-Driver/include/linux include/
+cp -R $ROCM_GIT_DIR/ROCK-Kernel-Driver/include/uapi include/
 
-cp -R ~/ROCm/ROCK-Kernel-Driver/drivers/gpu/drm/scheduler scheduler
-cp -R ~/ROCm/ROCK-Kernel-Driver/drivers/gpu/drm/ttm ttm
+cp -R $ROCM_GIT_DIR/ROCK-Kernel-Driver/drivers/gpu/drm/scheduler scheduler
+cp -R $ROCM_GIT_DIR/ROCK-Kernel-Driver/drivers/gpu/drm/ttm ttm
 
 
 
