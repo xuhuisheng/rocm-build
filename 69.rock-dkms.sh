3be4e5d0c79ba8029733ee1c992dd9c06c572365
xuhuisheng <xyz20003@gmail.com>
2021-01-21 17:51:35 +0800

    rock-dkms

--- 69.rock-dkms.sh
+++ 69.rock-dkms.sh
@@ -1,24 +1,52 @@
 
-mkdir -p $ROCM_BUILD_DIR/rock-dkms
+set -e
 
-cd $ROCM_BUILD_DIR/rock-dkms
+TARGET_DIR=$ROCM_BUILD_DIR/rock-dkms/usr/src/amdgpu-4.0-23
 
-cp -R $ROCM_GIT_DIR/ROCK-Kernel-Driver/drivers/gpu/drm/amd .
+mkdir -p $TARGET_DIR
 
-cp $ROCM_GIT_DIR/ROCK-Kernel-Driver/drivers/gpu/drm/amd/dkms/dkms.conf .
+cd $TARGET_DIR
 
-ln -s amd/dkms/Makefile Makefile
+cp -R $ROCM_GIT_DIR/ROCK-Kernel-Driver/drivers/gpu/drm/amd $TARGET_DIR
 
-mkdir -p firmware
-mkdir -p include
+# cp $ROCM_GIT_DIR/ROCK-Kernel-Driver/drivers/gpu/drm/amd/dkms/dkms.conf $TARGET_DIR
 
-cp -R $ROCM_GIT_DIR/ROCK-Kernel-Driver/include/drm include/
-cp -R $ROCM_GIT_DIR/ROCK-Kernel-Driver/include/kcl include/
-cp -R $ROCM_GIT_DIR/ROCK-Kernel-Driver/include/linux include/
-cp -R $ROCM_GIT_DIR/ROCK-Kernel-Driver/include/uapi include/
+ln -sf amd/dkms/Makefile $TARGET_DIR/Makefile
+ln -sf amd/dkms/dkms.conf $TARGET_DIR/dkms.conf
 
-cp -R $ROCM_GIT_DIR/ROCK-Kernel-Driver/drivers/gpu/drm/scheduler scheduler
-cp -R $ROCM_GIT_DIR/ROCK-Kernel-Driver/drivers/gpu/drm/ttm ttm
+mkdir -p $TARGET_DIR/firmware
+mkdir -p $TARGET_DIR/include
+
+# cp -R $ROCM_GIT_DIR/ROCK-Kernel-Driver/include/drm $DIST_DIR/include/
+# cp -R $ROCM_GIT_DIR/ROCK-Kernel-Driver/include/kcl $TARGET_DIR/include/
+# cp -R $ROCM_GIT_DIR/ROCK-Kernel-Driver/include/linux $TARGET_DIR/include/
+# cp -R $ROCM_GIT_DIR/ROCK-Kernel-Driver/include/uapi $TARGET_DIR/include/
+
+cp -R $ROCM_GIT_DIR/ROCK-Kernel-Driver/drivers/gpu/drm/scheduler $TARGET_DIR/scheduler/
+cp -R $ROCM_GIT_DIR/ROCK-Kernel-Driver/drivers/gpu/drm/ttm $TARGET_DIR/ttm/
+
+mkdir -p $TARGET_DIR/include/drm
+cp $ROCM_GIT_DIR/ROCK-Kernel-Driver/include/drm/amd_asic_type.h $TARGET_DIR/include/drm/
+cp $ROCM_GIT_DIR/ROCK-Kernel-Driver/include/drm/amd_rdma.h $TARGET_DIR/include/drm/
+cp $ROCM_GIT_DIR/ROCK-Kernel-Driver/include/drm/gpu_scheduler.h $TARGET_DIR/include/drm/
+cp $ROCM_GIT_DIR/ROCK-Kernel-Driver/include/drm/spsc_queue.h $TARGET_DIR/include/drm/
+mkdir -p $TARGET_DIR/include/drm/ttm
+cp -R $ROCM_GIT_DIR/ROCK-Kernel-Driver/include/drm/ttm/* $TARGET_DIR/include/drm/ttm/
+mkdir -p $TARGET_DIR/include/kcl
+cp -R $ROCM_GIT_DIR/ROCK-Kernel-Driver/include/kcl/* $TARGET_DIR/include/kcl/
+mkdir -p $TARGET_DIR/include/linux
+cp -R $ROCM_GIT_DIR/ROCK-Kernel-Driver/include/linux/dma-resv.h $TARGET_DIR/include/linux/
+cp -R $ROCM_GIT_DIR/ROCK-Kernel-Driver/include/kcl/reservation.h $TARGET_DIR/include/linux/
+mkdir -p $TARGET_DIR/include/uapi/drm
+cp $ROCM_GIT_DIR/ROCK-Kernel-Driver/include/uapi/drm/amdgpu_drm.h $TARGET_DIR/include/uapi/drm/
+mkdir -p TARGET_DIR/include/uapi/linux
+cp $ROCM_GIT_DIR/ROCK-Kernel-Driver/include/uapi/linux/kfd_ioctl.h $TARGET_DIR/include/uapi/linux/
+
+cd ../../..
+cp ../../rock-dkms_4.0-23_all . -R
+cp -R usr rock-dkms_4.0-23_all/
+
+dpkg -b rock-dkms_4.0-23_all
 
 
 
