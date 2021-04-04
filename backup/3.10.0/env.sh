93786f4ddd6502b925c4e6571f94818f4ee0b166
xuhuisheng <xuhuisheng@meicai.cn>
2021-04-04 08:31:43 +0800

    remove backup

--- backup/3.10.0/env.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-export ROCM_INSTALL_DIR=/opt/rocm
-export ROCM_GIT_DIR=/home/work/ROCm
-export ROCM_BUILD_DIR=/home/work/rocm-build/build
-export ROCM_PATCH_DIR=/home/work/rocm-build/patch
-export AMDGPU_TARGETS="gfx803;gfx900;gfx906"
-export PATH=$ROCM_INSTALL_DIR/bin:$ROCM_INSTALL_DIR/llvm/bin:$ROCM_INSTALL_DIR/hip/bin:$PATH
-
