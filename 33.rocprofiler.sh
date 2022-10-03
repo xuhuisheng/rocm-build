db2bfefba601dd44dbda071db306b8157392e621
xuhuisheng <xyz20003@gmail.com>
2022-10-03 20:39:14 +0800

    move rocprofiler before roctracer

--- 33.rocprofiler.sh
+++ 33.rocprofiler.sh
@@ -6,9 +6,15 @@ mkdir -p $ROCM_BUILD_DIR/rocprofiler
 cd $ROCM_BUILD_DIR/rocprofiler
 pushd .
 
+cd $ROCM_GIT_DIR/rocprofiler
+git reset --hard
+git apply $ROCM_PATCH_DIR/33.rocprofile-aql.patch
+cd $ROCM_BUILD_DIR/rocprofiler
+
 START_TIME=`date +%s`
 
 cmake \
+    -DPROF_API_HEADER_PATH=$ROCM_GIT_DIR/roctracer/inc/ext/ \
     -DCMAKE_INSTALL_PREFIX=$ROCM_INSTALL_DIR \
     -DCMAKE_BUILD_TYPE=Release \
     -DCPACK_PACKAGING_INSTALL_PREFIX=$ROCM_INSTALL_DIR \
--- env.sh
+++ env.sh
@@ -11,6 +11,6 @@ export ROCM_GIT_DIR=/home/work/ROCm
 export ROCM_BUILD_DIR=/home/work/rocm-build/build
 export ROCM_PATCH_DIR=/home/work/rocm-build/patch
 export AMDGPU_TARGETS="gfx803"
-export CMAKE_DIR=/home/work/local/cmake-3.16.8-Linux-x86_64
+export CMAKE_DIR=/home/work/local/cmake-3.18.6-Linux-x86_64
 export PATH=$ROCM_INSTALL_DIR/bin:$ROCM_INSTALL_DIR/llvm/bin:$ROCM_INSTALL_DIR/hip/bin:$CMAKE_DIR/bin:$PATH
 
