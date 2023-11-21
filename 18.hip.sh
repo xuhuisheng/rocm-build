60d7d857f2b07e45a173c16b5094453293480913
xuhuisheng <xyz20003@gmail.com>
2023-11-21 09:35:06 +0000

    test ROCm-5.7

--- 18.hip.sh
+++ 18.hip.sh
@@ -6,8 +6,8 @@ mkdir -p $ROCM_BUILD_DIR/hip
 cd $ROCM_BUILD_DIR/hip
 pushd .
 
-ROCclr_DIR=$ROCM_GIT_DIR/ROCclr
-OPENCL_DIR=$ROCM_GIT_DIR/ROCm-OpenCL-Runtime
+ROCclr_DIR=$ROCM_GIT_DIR/clr/rocclr
+OPENCL_DIR=$ROCM_GIT_DIR/clr/opencl
 HIP_DIR=$ROCM_GIT_DIR/HIP
 
 START_TIME=`date +%s`
@@ -15,19 +15,18 @@ START_TIME=`date +%s`
 cmake \
     -DOFFLOAD_ARCH_STR="--offload-arch=$AMDGPU_TARGETS" \
     -DHIP_COMMON_DIR="$HIP_DIR" \
-    -DAMD_OPENCL_PATH="$OPENCL_DIR" \
-    -DROCCLR_PATH="$ROCCLR_DIR" \
+    -DCLR_BUILD_HIP=ON \
     -DCMAKE_PREFIX_PATH="$ROCM_INSTALL_DIR" \
     -DCMAKE_BUILD_TYPE=Release \
     -DCPACK_GENERATOR=DEB \
-    -DROCM_PATCH_VERSION=50100 \
     -DCMAKE_INSTALL_PREFIX=$ROCM_BUILD_DIR/hip/install \
+    -DHIPCC_BIN_DIR="$ROCM_GIT_DIR/HIPCC/build" \
     -G Ninja \
-    $ROCM_GIT_DIR/hipamd
+    $ROCM_GIT_DIR/clr
 
 cmake --build .
 cmake --build . --target package
-sudo dpkg -i hip-dev*.deb hip-doc*.deb hip-runtime-amd*.deb hip-samples*.deb
+sudo dpkg -i hip-dev*.deb hip-doc*.deb hip-runtime-amd*.deb
 
 END_TIME=`date +%s`
 EXECUTING_TIME=`expr $END_TIME - $START_TIME`
